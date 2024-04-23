cd4d7ebaf3a154667b3d6c31f862b2e07561f33e
Guillermo Segura <correo@seguraguillermo.net>
2024-04-23 13:47:08 +0100

    feat: added data grid to movies component

--- src/components/movies/Movies.tsx
+++ src/components/movies/Movies.tsx
@@ -1,5 +1,11 @@
 import { useRef } from 'react';
 
+import { Box } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { Rating } from '../generic/rating/Rating';
+
+const PAGE_SIZE = 10;
+
 // TODO: use https://giddy-beret-cod.cyclic.app/movieCompanies
 const mockMovieCompanyData: any = [
   {id: "1", name: "Test Productions"},
@@ -16,22 +22,62 @@ export interface MoviesProps {
   setSelectedMovie: (movie: any) => void;
 }
 
+const columns: GridColDef<(typeof mockMovieData)[number]>[] = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  {
+    field: 'title',
+    headerName: 'Title',
+    width: 200,
+    editable: false,
+  },
+  {
+    field: 'filmCompanyId',
+    headerName: 'Company',
+    width: 200,
+    editable: false,
+    valueGetter: (value) => mockMovieCompanyData.find((f: any) => f.id === value)?.name
+  },
+  {
+    field: 'reviews',
+    headerName: 'Reviews',
+    width: 150,
+    editable: false,
+    renderCell: ({ value }) => <Rating values={value} />,
+  },
+  {
+    field: 'releaseYear',
+    headerName: 'Release year',
+    editable: false,
+    width: 100,
+  },
+];
+
 export const Movies = ({ setSelectedMovie , selectedMovie }: MoviesProps) =>  {
   const movieLength = useRef(mockMovieData.length);
   return (
     <div>
-      <p>Total movies displayed {movieLength.current}</p>
-      <span>Title - Review - Film Company</span>
-      <br />
-      <br />
-      {mockMovieData.map((movie: any) => 
-        <span onClick={() => {setSelectedMovie(movie)}}>
-          {movie.title}{" "}
-          {movie.reviews.reduce((acc: any, i: any) => (acc + i)/movie.reviews.length, 0)?.toString().substring(0, 3)}{" "}
-          {mockMovieCompanyData.find((f: any) => f.id === movie.filmCompanyId)?.name}
-          <br/>
-        </span>
-      )}
+      <h2>We've found {movieLength.current} movies in total</h2>
+      <p>Please select a movie to add a review</p>
+      <Box sx={{ width: '100%' }}>
+        <DataGrid
+          rows={mockMovieData}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: PAGE_SIZE,
+              },
+            },
+          }}
+          onRowSelectionModelChange={(selectedMovieId) => {
+            const movie = mockMovieData.find((movie) => movie.id === selectedMovieId[0]);
+            console.log(selectedMovieId, mockMovieData, movie);
+            setSelectedMovie(movie);
+          }}
+          checkboxSelection
+          disableMultipleRowSelection
+        />
+      </Box>
     </div>
   );
 }
