73d21b9cb9f5c45b1baf99c11522ebccaf0a1b92
Guillermo Segura <correo@seguraguillermo.net>
2024-04-23 22:51:40 +0100

    refactor: removed data context and typed context files

--- src/context/MovieCompaniesContext.tsx
+++ src/context/MovieCompaniesContext.tsx
@@ -1,17 +1,33 @@
-import createDataContext from './createDataContext';
+import React, { useReducer } from 'react';
+
+import { MovieCompany } from '../utils/types/models';
+import {
+  MovieCompaniesContextState,
+  MovieCompanyContextAction,
+  MovieCompanyContextActionTypes,
+  MovieCompaniesContext,
+} from '../utils/types/context';
 import moviesApi from '../api/moviesApi';
 
-const INITIAL_STATE = {
+const INITIAL_STATE: MovieCompaniesContextState = {
   movieCompanies: [],
   error: '',
 };
 
-const ACTION_TYPES = {
+const INITIAL_CONTEXT: MovieCompaniesContext = {
+  state: INITIAL_STATE,
+  fetchMovieCompanies: () => Promise.resolve(),
+}
+
+const ACTION_TYPES: MovieCompanyContextActionTypes = {
   setMovieCompanies: 'SET_MOVIE_COMPANIES_DATA',
   setError: 'SET_ERROR',
 };
 
-const movieCompaniesReducer = (state, action) => {
+const moviesReducer = (
+  state: MovieCompaniesContextState,
+  action: MovieCompanyContextAction,
+): MovieCompaniesContextState => {
   switch (action.type) {
     case ACTION_TYPES.setMovieCompanies:
       return { ...state, movieCompanies: action.payload, error: '' };
@@ -22,8 +38,10 @@ const movieCompaniesReducer = (state, action) => {
   }
 };
 
-const fetchMovieCompanies = (dispatch) => async () => {
-  await moviesApi.get('/movieCompanies').then((res) => {
+const fetchMovieCompanies = (
+  dispatch: React.Dispatch<MovieCompanyContextAction>
+) => async (): Promise<void> => {
+  await moviesApi.get<MovieCompany[]>('/movieCompanies').then((res) => {
     dispatch({
       type: ACTION_TYPES.setMovieCompanies,
       payload: res.data,
@@ -37,12 +55,18 @@ const fetchMovieCompanies = (dispatch) => async () => {
   });
 };
 
-const movieCompaniesActions = {
-  fetchMovieCompanies,
-};
+export const Context = React.createContext<MovieCompaniesContext>(INITIAL_CONTEXT);
 
-export const { Provider, Context } = createDataContext(
-  movieCompaniesReducer,
-  movieCompaniesActions,
-  INITIAL_STATE,
-);
+export const Provider = ({ children }: { children: React.ReactNode }) => {
+  const [state, dispatch] = useReducer(moviesReducer, INITIAL_STATE);
+
+  const actions = {
+    fetchMovieCompanies: fetchMovieCompanies(dispatch),
+  };
+
+  return (
+    <Context.Provider value={{ state, ...actions }}>
+      {children}
+    </Context.Provider>
+  );
+};
--- src/context/MoviesContext.tsx
+++ src/context/MoviesContext.tsx
@@ -1,19 +1,36 @@
-import createDataContext from './createDataContext';
+import React, { useReducer } from 'react';
+
+import { Movie } from '../utils/types/models';
+import {
+  MoviesContextState,
+  MoviesContextAction,
+  MoviesContextActionTypes,
+  MoviesContext,
+} from '../utils/types/context';
 import moviesApi from '../api/moviesApi';
 
-const INITIAL_STATE = {
+const INITIAL_STATE: MoviesContextState = {
   movies: [],
   selectedMovie: undefined,
   error: '',
 };
 
-const ACTION_TYPES = {
+const INITIAL_CONTEXT: MoviesContext = {
+  state: INITIAL_STATE,
+  fetchMovies: () => Promise.resolve(),
+  selectMovie: () => null,
+}
+
+const ACTION_TYPES: MoviesContextActionTypes = {
   setMovies: 'SET_MOVIES_DATA',
   setError: 'SET_ERROR',
   setSelectedMovie: 'SET_SELECTED_MOVIE',
 };
 
-const moviesReducer = (state, action) => {
+const moviesReducer = (
+  state: MoviesContextState,
+  action: MoviesContextAction,
+): MoviesContextState => {
   switch (action.type) {
     case ACTION_TYPES.setMovies:
       return { ...state, movies: action.payload, error: '' };
@@ -26,8 +43,11 @@ const moviesReducer = (state, action) => {
   }
 };
 
-const fetchMovies = (dispatch) => async () => {
-  await moviesApi.get('/movies').then((res) => {
+const fetchMovies = (
+  dispatch: React.Dispatch<MoviesContextAction>
+) => async (): Promise<void> => {
+  await moviesApi.get<Movie[]>('/movies')
+  .then((res) => {
     dispatch({
       type: ACTION_TYPES.setMovies,
       payload: res.data,
@@ -41,20 +61,31 @@ const fetchMovies = (dispatch) => async () => {
   });
 };
 
-const selectMovie = (dispatch) => (movie) => {
+const selectMovie = (
+  dispatch: React.Dispatch<MoviesContextAction>
+) => (movie: Movie | undefined): void => {
   dispatch({
     type: ACTION_TYPES.setSelectedMovie,
     payload: movie,
   })
 };
 
-const moviesActions = {
-  fetchMovies,
-  selectMovie,
+export const Context = React.createContext<MoviesContext>(INITIAL_CONTEXT);
+
+export const Provider = ({ children }: { children: React.ReactNode }) => {
+  const [state, dispatch] = useReducer(moviesReducer, INITIAL_STATE);
+
+  const actions = {
+    fetchMovies: fetchMovies(dispatch),
+    selectMovie: selectMovie(dispatch),
+  };
+
+  const value = { state, ...actions };
+  return (
+    <Context.Provider value={value}>
+      {children}
+    </Context.Provider>
+  );
 };
 
-export const { Provider, Context } = createDataContext(
-  moviesReducer,
-  moviesActions,
-  INITIAL_STATE,
-);
+
