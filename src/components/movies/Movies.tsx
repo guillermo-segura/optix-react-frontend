ec88eb7a5aa15355e8a8fd7ec3f9e46d044e9138
Guillermo Segura <correo@seguraguillermo.net>
2024-04-23 17:54:20 +0100

    refactor: moved logic from movies into separate hook

--- src/components/movies/Movies.tsx
+++ src/components/movies/Movies.tsx
@@ -1,11 +1,8 @@
-import { useEffect, useContext } from 'react';
-
 import { Box } from '@mui/material';
 import { RefreshButton } from '../refreshButton/RefreshButton';
 import { MoviesHeader } from '../moviesHeader/MoviesHeader';
-import { Context as MoviesContext } from '../../context/MoviesContext';
-import { Context as MovieCompaniesContext } from '../../context/MovieCompaniesContext';
 import { MoviesTable } from '../moviesTable/MoviesTable';
+import { useMoviesData } from '../../hooks/useMoviesData/useMoviesData';
 
 export interface MoviesProps {
   selectedMovie?: any;
@@ -13,22 +10,15 @@ export interface MoviesProps {
 }
 
 export const Movies = () =>  {
-  const { fetchMovies } = useContext(MoviesContext);
-  const { fetchMovieCompanies } = useContext(MovieCompaniesContext);
-
-  useEffect(() => {
-    fetchMovieCompanies();
-    fetchMovies();
-  }, []);
-
+  useMoviesData();
   return (
-    <div>
+    <>
       <MoviesHeader />
       <Box display="flex" flexDirection="row" alignItems="center" justifyContent="space-between">
-        <p>Please select a movie to add a review</p>
+        <p>Please select a movie to add your review</p>
         <RefreshButton />
       </Box>
       <MoviesTable />
-    </div>
+    </>
   );
 }
