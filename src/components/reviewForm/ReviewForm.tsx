4c4d0102d576ec7e0da826ce0e7d9992615964e6
Guillermo Segura <correo@seguraguillermo.net>
2024-04-23 17:09:34 +0100

    feat: updated review form adding material components

--- src/components/reviewForm/ReviewForm.tsx
+++ src/components/reviewForm/ReviewForm.tsx
@@ -1,20 +1,55 @@
-export interface ReviewFormProps {
-  selectedMovie: {
-    title?: string;
-  },
-}
+import { Avatar, Card, CardContent, CardHeader, TextField, CardActions, Button } from "@mui/material";
+import { useState, useContext } from "react";
+import { Rating } from "../generic/rating/Rating";
+import { Context as MovieContext } from '../../context/MoviesContext';
 
-export const ReviewForm = ({ selectedMovie }: ReviewFormProps) => {
-  return (
-    <div>
-      <p>{selectedMovie.title ? "You have selected " +  selectedMovie.title : "No movie title"}</p>
-      <p>Please leave a review below</p>
-      <form onSubmit={() => {}}>
-        <label>
-        Review:
-        <input type="text"/>
-      </label>
-      </form>
-    </div>
+
+export const ReviewForm = () => {
+  const { state } = useContext(MovieContext);
+  const [description, setDescription] = useState('');
+  const [rating, setRating] = useState(0);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log('Submit clicked', description, rating);
+  };
+
+  const selectedMovie = state.selectedMovie;
+
+  return selectedMovie && (
+    <form onSubmit={onSubmit}>
+      <Card sx={{
+        marginTop: '16px',
+      }}>
+        <CardHeader
+          title={selectedMovie.title}
+          subheader="[INSERT COMPANY HERE]"
+          avatar={
+            <Avatar aria-label="recipe">
+              {selectedMovie.title?.split('')[0]}
+            </Avatar>
+          }
+        />
+        <CardContent>
+            <p>Please provide some feedback about the movie</p>
+            <Rating values={[rating]} onChange={(e, newValue) => setRating(newValue * 2)}/>
+            <br />
+            <TextField
+              id="outlined-textarea"
+              label="Multiline Placeholder"
+              placeholder="Placeholder"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              minRows={4}
+              multiline
+            />
+        </CardContent>
+
+        <CardActions>
+          <Button>Cancel</Button>
+          <Button type="submit" variant="contained">Submit</Button>
+        </CardActions>
+      </Card>
+    </form>
   );
 };
