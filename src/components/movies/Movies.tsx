88ddeb908508390dc345b964eb542b8518c1461e
Guillermo Segura <correo@seguraguillermo.net>
2024-04-24 09:44:14 +0100

    refactor: removed movies header component

--- src/components/movies/Movies.tsx
+++ src/components/movies/Movies.tsx
@@ -1,19 +1,34 @@
 import { Box, Typography } from '@mui/material';
 
-import { MoviesHeader } from '../moviesHeader/MoviesHeader';
 import { MoviesTable } from '../moviesTable/MoviesTable';
 import { RefreshMoviesButton } from '../refreshMoviesButton/RefreshMoviesButton';
 import { useMoviesData } from '../../hooks/useMoviesData/useMoviesData';
 import { ReviewForm } from '../reviewForm/ReviewForm';
 
+const getHeaderContent = (count: number, loading: boolean): string => {
+  if (count > 1) {
+    return `We found ${count} movies`;
+  }
+
+  if (count === 1) {
+    return 'We found 1 movie';
+  }
+
+  return loading ? 'Loading' : 'We found 0 movies';
+};
+
 export const Movies = () =>  {
-  const { selectedMovie } = useMoviesData();
+  const { selectedMovie, loading, movies } = useMoviesData();
+  const isLoadingState = movies.length < 1 && loading;
+
   return (
     <>
-      <MoviesHeader />
+      <Typography variant="h4" component="h4" sx={{ padding: '24px 0' }}>
+        {getHeaderContent(movies.length, isLoadingState)}
+      </Typography>
       <Box display="flex" flexDirection="row" alignItems="center" justifyContent="space-between">
         <Typography variant="body1" component="p">
-          Please select a movie to add your review
+          {isLoadingState ? 'Please wait while we load our movies' : 'Select a movie to add your review'}
         </Typography>
         <RefreshMoviesButton />
       </Box>
