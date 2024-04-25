2a533e23b4c64fb9476c5a70cc160d1697a067f6
Guillermo Segura <correo@seguraguillermo.net>
2024-04-25 12:40:19 +0100

    test: added app test

---
+++ src/components/app/App.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from '@testing-library/react';
+import { App } from './App';
+
+jest.mock('../notificationsContainer/NotificationsContainer', () => ({
+  __esModule: true,
+  NotificationsContainer: jest.fn(() => <div data-testid="notifications-container">Mocked Notifications Container</div>)
+}));
+
+jest.mock('../movies/Movies', () => ({
+  __esModule: true,
+  Movies: jest.fn(() => <div data-testid="movies">Mocked Movies Component</div>)
+}));
+
+jest.mock('../../context/MoviesContext', () => ({
+  MoviesProvider: ({ children }: { children: JSX.Element }) => children,
+}));
+
+jest.mock('../../context/MovieCompaniesContext', () => ({
+  MovieCompaniesProvider: ({ children }: { children: JSX.Element }) => children,
+}));
+
+describe('App component', () => {
+  it('renders without crashing', () => {
+    render(<App />);
+  });
+
+  it('displays the welcome message', () => {
+    render(<App />);
+    const welcomeMessage = screen.getByText('Welcome to MovieDB!');
+    expect(welcomeMessage).toBeInTheDocument();
+  });
+
+  it('renders Movies component with correct props', () => {
+    render(<App />);
+    const moviesComponent = screen.getByTestId('movies');
+    expect(moviesComponent).toBeInTheDocument();
+  });
+
+  it('renders NotificationsContainer component with correct props', () => {
+    render(<App />);
+    const moviesComponent = screen.getByTestId('notifications-container');
+    expect(moviesComponent).toBeInTheDocument();
+  });
+});
+
